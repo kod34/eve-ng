399361946fa41e8363085cfd2df499a00de3e310
kod34 <83618832+kod34@users.noreply.github.com>
2022-03-09 04:59:06 -0500

    Update vmdk2qcow.sh

--- vmdk2qcow.sh
+++ vmdk2qcow.sh
@@ -6,6 +6,7 @@ FILE=/opt/unetlab/addons/qemu/$1/$1
 if test -f "$FILE"; then
     mv /opt/unetlab/addons/qemu/$1/$1 /opt/unetlab/addons/qemu/$1/$1.vmdk
     /opt/qemu/bin/qemu-img convert -f vmdk -O qcow2 /opt/unetlab/addons/qemu/$1/$1.vmdk /opt/unetlab/addons/qemu/$1/$1.qcow2
+    mv /opt/unetlab/addons/qemu/$1/$1.qcow2 /opt/unetlab/addons/qemu/$1/virtioa.qcow2
     rm /opt/unetlab/addons/qemu/$1/$1.vmdk
     /opt/unetlab/wrappers/unl_wrapper -a fixpermissions
 fi
